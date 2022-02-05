a8b67841ab1dd0325ec4a36050198f3927d86a01
kanishka-28 <kanishka.gour.mat20@itbhu.ac.in>
2022-02-05 11:25:44 +0530

    auth updated

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const db = require("./database/connection/index");
 
 const express = require("express");
 const cors = require("cors")
-const auth = require("./routes/auth/index")
+const auth = require("./routes/auth")
 const user = require("./routes/user/index")
 const resume = require("./routes/resume/index")
 const intern = require("./routes/intern/index")
